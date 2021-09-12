bfa30840d3c7e5306b810dad26a758b0f7b19e7f
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-12 16:09:05 -0400

    Initial commit

---
+++ src/main/java/Exercise14.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Magdalena Sobrino-Almanzar
+ */
+
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args){
+
+    int orderAmount;
+    double total, stateTax = 0;
+    String state;
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("What is the order amount? ");
+    orderAmount =input.nextInt();
+    System.out.print("What is the State? ");
+    state =input.next();
+
+
+    if(state.equals("WI"))
+
+    {
+        stateTax = orderAmount * 0.055;
+    }
+
+    total=orderAmount+stateTax;
+
+    System.out.printf("The subtotal is $%.2f \n",(double)orderAmount);
+    System.out.printf("The tax is $%.2f \n",stateTax);
+    System.out.printf("The total is $%.2f \n",total);
+
+
+ }
+}
